98ec5f661bde7cf61aa47c5fd7bb4fae18837b03
Carlos Barragan <carlos.brgan@gmail.com>
2025-09-09 19:58:13 +1000

    feat: Add Person model and related DTOs for person management
    This commit introduces a new domain model class `Person` with properties such as `PersonID`, `PersonName`, `Email`, `DateOfBirth`, `Gender`, `CountryID`, `Address`, and `ReceiveNewsLetters`.
    
    A new DTO class `PersonAddRequest` is added for creating a new person, including a method to convert it to a `Person` object.
    
    The `PersonResponse` class is updated to serve as the return type for the Persons Service, with overridden equality methods and an extension method for conversion from `Person`.
    
    Additionally, an enumeration `GenderOptions` is introduced to represent gender options in the `PersonAddRequest`.

---
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -0,0 +1,38 @@
+﻿using Entities;
+using ServiceContracts.Enums;
+using System;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// Acts as a DTO for inserting a new person
+    /// </summary>
+    public class PersonAddRequest
+    {
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public GenderOptions? Gender { get; set; }
+        public Guid? CountryID { get; set; }
+        public bool? ReceiveNewsLetters { get; set; }
+
+
+        /// <summary>
+        /// Converts the current object of PersonAddRequest into a new object of Person
+        /// type PersonResponse
+        /// </summary>
+        /// <returns></returns>
+        public Person ToPerson()
+        {
+            return new Person()
+            {
+                PersonName = PersonName,
+                Email = Email,
+                DateOfBirth = DateOfBirth,
+                Gender = Gender.ToString(),
+                CountryID = CountryID,
+                ReceiveNewsLetters = ReceiveNewsLetters
+            };
+        }
+    }
+}
